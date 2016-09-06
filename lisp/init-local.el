dfce63ba1167d3b05e823d66736492fe18dfa5c5
unknown <Jeff.Ruan@NZ171.px.dps.local>
2016-09-07 08:45:59 +1200

    use 100M gc-cons-threshold

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,6 +5,8 @@
 (defvar savefile-dir (expand-file-name "savefile" user-emacs-directory)
   "This folder stores all the automatically generated save/history-files.")
 
+(setq gc-cons-threshold 100000000) ; ie 100mb, default is 800kb
+
 (require 'setup-editing)
 (require 'setup-helm)
 (require 'setup-helm-projectile)
